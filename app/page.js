b183d8f4e53a9b0030a4449f09a3e1c18751ae12
FRCroboSam <samuelwang787@gmail.com>
2023-12-02 16:22:31 -0800

    scuffed map

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
-
-
+"use client";
+import MyMap from "./components/map"
+import React from "react";
 
 export default function Home() {
   return (
     <div class="h-full">
+      <MyMap/>
       <p>This is the home page!</p>
     </div>
   );
