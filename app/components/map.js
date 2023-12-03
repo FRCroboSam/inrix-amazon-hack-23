006b35513afb169beaed2c61de7eb5fc09385af5
Saravanan Sonia <srsonia98@yahoo.com.sg>
2023-12-02 17:31:03 -0800

    map appeared! yay

--- app/components/map.js
+++ app/components/map.js
@@ -2,14 +2,15 @@
 
 import React from 'react';
 import { MapContainer, Marker, TileLayer, Tooltip } from "react-leaflet"
-// import "./leaflet.css"
-// import "leaflet-defaulticon-compatibility"
-// import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
+import 'leaflet/dist/leaflet.css';
+//import "./leaflet.css"
+//import "leaflet-defaulticon-compatibility"
+//import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
 
 export default function MyMap() {
   const position = [52.105, 109]
   const zoom = 0.5
-  return (<MapContainer center={position} zoom={zoom} scrollWheelZoom={false}>
+  return (<MapContainer center={position} zoom={zoom} scrollWheelZoom={false} style={{ height: '400px', width: '100%' }}>
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
