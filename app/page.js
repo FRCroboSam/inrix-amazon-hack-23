24594045dad8eab7b511e21483cb7193c48fb5a5
Keira <1keira.kw@gmail.com>
2023-12-02 18:47:12 -0800

    added a side bar

--- app/page.js
+++ app/page.js
@@ -1,12 +1,19 @@
 "use client";
 import MyMap from "./components/map"
 import React from "react";
+import Sidebar from './components/sidebar.jsx';
 
 export default function Home() {
   return (
     <div class="h-full">
+      
       <MyMap/>
-      <p>This is the home page!</p>
+      <Sidebar/>
+      <div class="bg-gray-200 w-1/4 p-4">
+        <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+        <h2 class="text-lg font-semibold mb-4">Sidebar</h2>
+        <p>Some sidebar content...</p>
+    </div>
     </div>
   );
 }
