67a4719e41d1f886662e06ac43513b44bf57f9cd
fgonza3 <fgonza2@uw.edu>
2023-12-02 18:34:58 -0800

    update map.js

--- app/components/map.js
+++ app/components/map.js
@@ -1,24 +1,30 @@
 "use client";
 
 import React from 'react';
-import { MapContainer, Marker, TileLayer, Tooltip } from "react-leaflet"
+import { MapContainer, Marker, TileLayer, Tooltip, Popup, Polyline } from "react-leaflet"
 import 'leaflet/dist/leaflet.css';
 //import "./leaflet.css"
 //import "leaflet-defaulticon-compatibility"
 //import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
 
 export default function MyMap() {
-  const position = [52.105, 109]
-  const zoom = 0.5
-  return (<MapContainer center={position} zoom={zoom} scrollWheelZoom={false} style={{ height: '400px', width: '100%' }}>
+  const position = [37.7749, -122.4194]
+  const zoom = 10
+  const polyline = [
+    [37.7749, -122.4194],
+    [38.0648, -122.4194],
+  ]
+  const puprpleOptions = {color: 'purple'}
+  return (<MapContainer center={position} zoom={zoom} scrollWheelZoom={false} style={{ height: '100vh', width: '100%', position: 'absolute', top: 0, left: 0 }}>
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    {/* <Marker position={position}> */}
-      {/* <Popup>
+    { <Marker position={position}> 
+      {<Popup>
         A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup> */}
-    {/* </Marker> */}
+      </Popup> }
+    </Marker> }
+    <Polyline pathOptions ={puprpleOptions} positions = {polyline} />
   </MapContainer>)
 }
