4ced0a6a4c44052977f4270cac3e3519693cff07
Keira <1keira.kw@gmail.com>
2023-12-02 17:59:16 -0800

    updated the fuel api key request

--- app/api/data.js
+++ app/api/data.js
@@ -10,8 +10,7 @@ var requestOptions = {
   redirect: 'follow'
 };
 
-fetch("https://fuel-api.inrix.com/v1/fuelstations/?point=47.61121|-122.32538&radius=2&accesstoken=YXphbGUycnV4aHxvTFBmamVLVUl2OG5DbHIzVEJuWjBhNlV4eksyWmpZQjZ6RTB6a3pF", requestOptions)
+fetch("https://places.googleapis.com/v1/places/ChIJj61dQgK6j4AR4GeTYWZsKWw?fields=id,displayName&key=AIzaSyC5yg2CgvpTYkNjntXXpwMNSTLkUkvkKSc", requestOptions)
   .then(response => response.text())
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
-
