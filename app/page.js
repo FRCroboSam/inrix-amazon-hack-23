95f0d02020b907235afa9b7c9a375c2abdb9264f
FRCroboSam <samuelwang787@gmail.com>
2023-12-02 22:01:07 -0800

    PUSHHH

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
 "use client";
 import MyMap from "./components/map"
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import Sidebar from './components/sidebar.jsx';
 
 export default function Home() {
+    useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div class="h-full">
       
@@ -18,3 +21,41 @@ export default function Home() {
     </div>
   );
 }
+const fetchData = async () => {
+  try {
+    const response = await fetch("http://localhost:3001/findRoute"); // Replace "api/data" with the actual endpoint of your backend
+    const result = await response.json();
+    const wayPoint1 = result.json2.result.trip.wayPoints[0].geometry.coordinates[0];
+    console.log("WAYPOINT 1: " + wayPoint1);
+    alert(wayPoint1)
+  } catch (error) {
+    alert("meep", error)
+    console.error("Error fetching data:", error);
+  }
+};
+
+const extractPointsFromJson = (jsonData) => {
+  alert("CORDS"  + coordinatesArray)
+
+  const wayPoint1 = jsonData.json2.result.trip.wayPoints[0].geometry.coordinates[0];
+  console.log("WAYPOINT 1: " + wayPoint1);
+
+
+
+  const wayPoint2 = jsonData.json2.result.trip.wayPoints;
+
+  const coordinatesArray = wayPoints.map(wayPoint => {
+    const geometry = wayPoint.geometry;
+    const coordinates = geometry.coordinates[0];
+
+    return {
+      id: wayPoint.id,
+      coordinates: {
+        longitude: coordinates[0],
+        latitude: coordinates[1]
+      }
+    };
+  });
+
+  return coordinatesArray;
+};
