afb5eaa9ee0a953215638efc2ae5b709f46384d7
FRCroboSam <samuelwang787@gmail.com>
2023-12-02 16:32:41 -0800

    SHowing the map scuffed

--- app/components/map.js
+++ app/components/map.js
@@ -2,22 +2,22 @@
 
 import React from 'react';
 import { MapContainer, Marker, TileLayer, Tooltip } from "react-leaflet"
-// import "leaflet/dist/leaflet.css"
+// import "./leaflet.css"
 // import "leaflet-defaulticon-compatibility"
 // import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
 
 export default function MyMap() {
   const position = [52.105, 109]
-  const zoom = 0.3
+  const zoom = 0.5
   return (<MapContainer center={position} zoom={zoom} scrollWheelZoom={false}>
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    {/* <Marker position={position}>
-      <Popup>
+    {/* <Marker position={position}> */}
+      {/* <Popup>
         A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker> */}
+      </Popup> */}
+    {/* </Marker> */}
   </MapContainer>)
 }
